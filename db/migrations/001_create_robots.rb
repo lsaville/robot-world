fc2af620390fdaf600fdf7ae47a7767ba3bd467e
Lee Saville <l.w.saville@gmail.com>
2016-10-04 15:52:01 -0600

    Initial commit

---
+++ db/migrations/001_create_robots.rb
@@ -0,0 +1,9 @@
+require 'sqlite3'
+
+database = SQLite3::Database.new("db/robot_world_development.db")
+database.execute("CREATE TABLE robots (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                       title VARCHAR(64),
+                                       description VARCHAR(64)
+                                       );"
+                )
+puts "creating robots table for development"                
