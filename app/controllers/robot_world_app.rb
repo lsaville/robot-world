d83745bbd48dc31894609868c3e268dc7ee453af
Lee Saville <l.w.saville@gmail.com>
2016-10-04 16:15:42 -0600

    Add '/' method to prove it works

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,8 +3,11 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+  get '/' do
+    "Hello Robot World!!!"
+  end
+
   get '/robots' do
     erb :index
   end
 end
-
