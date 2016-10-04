9ab99a8713a0063b2c31f419605344016f11c49e
Lee Saville <l.w.saville@gmail.com>
2016-10-04 16:56:32 -0600

    Add the seven methods corresponding to CRUD

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -3,11 +3,51 @@ require_relative '../models/robot.rb'
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
+#dashboard... an intro to Robot World
   get '/' do
     erb :dashboard
   end
 
+#See all Robots
   get '/robots' do
+    @robots = Robot.all
     erb :index
+  end
+
+#See one robot
+  get 'robots/:id' do
+    @robots = Robot.find(id)
+    erb :show
+  end
+
+#See form to make new robot
+  get 'robots/new' do
+    erb :new
+  end
+
+#submit the new robot
+  post '/robots' do
+    robot = Robot.new(params[:robot])
+    task.save
+    redirect '/robots'
+  end
+
+#see form to edit robot
+  get 'robots/:id/edit'
+    @robot = Robot.find(params[:id])
+    erb :edit
+  end
+
+#submit the new robot info
+  set :method_override, true
+  put 'robots/:id' do |id|
+    Robot.update(id.to_i, params[:task])
+    redirect "robots/#{id}"
+  end
+
+#delete robot
+  delete 'robots/:id' do |id|
+    Robot.destroy(id.to_i)
+    redirect '/tasks'
   end
 end
