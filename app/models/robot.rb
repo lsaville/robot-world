0007d05235b23eb10949db59ee2839b574b2ee0c
Lee Saville <l.w.saville@gmail.com>
2016-10-04 16:13:52 -0600

    Add instance variables to Robot

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -2,7 +2,12 @@ require 'sqlite3'
 
 class Robot
   def initialize(robot_params)
+    @name = robot_params["name"]
+    @city = robot_params["city"]
+    @state = robot_params["state"]
+    @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 end
