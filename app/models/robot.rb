15625ba189c91c337c8071cb07db56c731c6ce12
Lee Saville <l.w.saville@gmail.com>
2016-10-04 18:02:57 -0600

    Add attr_reader. set results_as_hash to true instead of nothing

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 
 class Robot
+  attr_reader :name, :city, :state, :department, :id
   def initialize(robot_params)
     @name = robot_params["name"]
     @city = robot_params["city"]
@@ -8,10 +9,24 @@ class Robot
     @department = robot_params["department"]
     @database = SQLite3::Database.new('db/robot_world_development.db')
     @database.results_as_hash = true
-    @id = task_params["id"] if task_params["id"]
+    @id = robot_params["id"] if robot_params["id"]
   end
 
   def save
-    @database.execute("INSERT INTO tasks (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+    @database.execute("INSERT INTO robots (name, city, state, department) VALUES (?, ?, ?, ?);", @name, @city, @state, @department)
+  end
+
+  def self.all
+    self.database
+    robots = database.execute("SELECT * FROM robots")
+    robots.map do |robot|
+      Robot.new(robot)
+    end
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/robot_world_development.db')
+    database.results_as_hash = true
+    database
   end
 end
